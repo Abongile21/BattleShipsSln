98228f7f12a297ff723ba6da4b70ef1244924ccb
Abongile21 <abongiletheo00@gmail.com>
2025-05-15 15:15:42 +0200

    Add project files.

---
+++ BattleShipsSln/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleShipsSln
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
